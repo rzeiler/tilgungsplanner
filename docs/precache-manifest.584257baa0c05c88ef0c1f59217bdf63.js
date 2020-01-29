4f553ce7f47dd852cb83a1d217b0aec77e57b9bb
ralf <ralf>
2020-01-29 14:52:03 +0100

    build

---
+++ docs/precache-manifest.584257baa0c05c88ef0c1f59217bdf63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18dff21c88ec96644d28",
+    "url": "/docs/css/app.e7c256ed.css"
+  },
+  {
+    "revision": "40e93b11cbd4174c60b22cba0d630300",
+    "url": "/docs/css/normalize.css"
+  },
+  {
+    "revision": "6a1ca96f1a1a43840ab6a91211646a5b",
+    "url": "/docs/index.html"
+  },
+  {
+    "revision": "18dff21c88ec96644d28",
+    "url": "/docs/js/app.e8601af9.js"
+  },
+  {
+    "revision": "39777dd2b2709c8fe032",
+    "url": "/docs/js/chunk-vendors.142e0455.js"
+  },
+  {
+    "revision": "4e74ea7d979cf881848df5ae40a9e2e4",
+    "url": "/docs/manifest.json"
+  },
+  {
+    "revision": "1d27247c24f327bb1ccb631e7fdeac93",
+    "url": "/docs/public/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/docs/robots.txt"
+  }
+]);
